e536179edd8596facfda74f03873ce372e3e006c
= <=>
2021-02-27 20:40:35 +0530

    new changes

--- static/storykey/button_script.js
+++ static/storykey/button_script.js
@@ -7,3 +7,12 @@ $(".boton").wrapInner('<div class=botontext></div>');
     
     $(".twist").css("width", "25%").css("width", "+=3px");
 });
+// if no Webkit browser
+(function(){
+    let isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+    let isSafari = /Safari/.test(navigator.userAgent) && /Apple Computer/.test(navigator.vendor);
+    let scrollbarDiv = document.querySelector('.scrollbar');
+      if (!isChrome && !isSafari) {
+        scrollbarDiv.innerHTML = 'You need Webkit browser to run this code';
+      }
+  })();
